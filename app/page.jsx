269a599eca178de2dcf94f48062db4cb45e97425
CICEROsandbox <reed@cicero.oslo.no>
2024-12-11 17:52:04 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,9 @@
+import TranslationInterface from '../components/TranslationInterface'
 
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <TranslationInterface />
+    </main>
+  )
+}

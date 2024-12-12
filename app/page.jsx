935ecf46070b931a9f5ff5a7fb89931fb788d2f2
CICEROsandbox <reed@cicero.oslo.no>
2024-12-12 16:30:18 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       updatedParagraphs[index].isTranslating = true;
       setParagraphs(updatedParagraphs);
 
-      const response = await fetch('/api/translate', {
+      const response = await fetch('/api/translate/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: paragraphs[index].norwegian })
